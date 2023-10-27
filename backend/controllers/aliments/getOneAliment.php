41a3c85fb55635f3cee7df8343c66677ef7de238
Gabriel Ivanes <ivanes.gabriel@gmail.com>
2023-10-27 15:13:52 +0200

    backend modification

---
+++ backend/controllers/aliments/getOneAliment.php
@@ -0,0 +1,51 @@
+<?php
+require_once('../../init_pdo.php');
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+
+$data = json_decode(file_get_contents("php://input"));
+
+if(!empty($data->id_aliment) ) {
+    $id = $data->id_aliment;
+
+    $sql = "SELECT * FROM ALIMENT WHERE ID = :id";
+
+    $request = $pdo->prepare($sql);
+    $id = htmlspecialchars(strip_tags($id));
+    $request->bindParam(":id", $id);
+
+    $request->execute();
+
+    $num = $request->rowCount();
+
+    if ($num > 0) {
+
+        $row = $request->fetch(PDO::FETCH_ASSOC);
+        extract($row);
+
+       $aliment_item = array(
+            "id"=>$ID,
+            "categorie_id"=>$CAT_ID,
+            "nom"=>$NOM
+
+        );
+        
+        http_response_code(200);
+  
+        echo json_encode($aliment_item);
+    } else {
+        http_response_code(503);
+  
+        echo json_encode(array("message" => "Aucun aliment trouvé."));
+    }
+} else {
+    http_response_code(400);
+
+    echo json_encode(array("message" => "Le champs demandé n'est pas renseigné."));
+}
+
+?>
---
+++ backend/init_db.php
@@ -0,0 +1,28 @@
+<?php
+require_once('config.php');
+
+$connectionString = "mysql:host=". _MYSQL_HOST;
+if(defined('_MYSQL_PORT')) $connectionString .= ";port=". _MYSQL_PORT;
+$connectionString .= ";dbname=" . _MYSQL_DBNAME;
+
+$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' );
+
+$pdo = NULL;
+
+try {
+    $pdo = new PDO($connectionString, _MYSQL_USER, _MYSQL_PASSWORD, $options);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sqlFile = file_get_contents('./sql/database.sql');
+    $pdo -> exec($sqlFile);
+    
+    echo 'Connexion réussie';
+}
+catch (PDOException $erreur) {
+    echo 'Erreur : '.$erreur->getMessage();
+}
+
+
+$pdo = null;
+
+?>
